34d88f18ed134fc1392a5af60e258e5c91c780c5
Edvvrdd <edward.y.dai@gmail.com>
2023-12-01 13:37:51 -0500

    Starting to make the level manager

--- Scripts/instrument.gd
+++ Scripts/instrument.gd
@@ -1,14 +1,20 @@
 extends Node
 
 
-var Instrument_name: String = "Unnamed Instrument"
-var volume: float = 1.0
+var instrument_name: String = "Unnamed Instrument"
+var keys = [] # Array to store the keys for the instrument
 
-func _init(instrument_name: String):
-	Instrument_name = instrument_name
+func _init(name: String, keys_array):
+	instrument_name = name
+	keys = keys_array
+	set_process_input(true)
 
-func play_note(note: String):
-	print(Instrument_name + " is playing note: " + note)
+func _input(event):
+	if event is InputEventKey and event.is_pressed():
+		for i in range(keys.size()):
+			if event.scancode == keys[i]:
+				play_note(i)
 
-func stop_playing():
-	print(Instrument_name + " stopped playing")
+func play_note(note_index: int):
+	print(instrument_name + " plays note number: " + str(note_index))
+	# This method will be overridden in child classes
